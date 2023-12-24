2a1a9b029309c023ec624da78b411a9256c83620
cefe!s <fireinspirer@gmail.com>
2023-12-25 01:58:55 +0300

    added needed sql query

--- src/main/java/util/constants/SqlQueryConstants.java
+++ src/main/java/util/constants/SqlQueryConstants.java
@@ -10,6 +10,7 @@ public class SqlQueryConstants {
 
     public static String SQL_GET_USER_BY_ID = "SELECT * FROM \"reflection-task\".users WHERE id = ?";
     public static String SQL_GET_ALL_USERS = "SELECT * FROM \"reflection-task\".users";
+    public static String SQL_GET_ALL_USERS_PAGED = "SELECT * FROM \"reflection-task\".users LIMIT ? OFFSET ?";
     public static String SQL_CREATE_USER = "INSERT INTO \"reflection-task\".users (name, surname, email, age) VALUES ( ?, ?, ?, ?)";
     public static String SQL_UPDATE_USER = "UPDATE \"reflection-task\".users SET name = ?, surname = ?, email = ?, age = ? WHERE id = ?";
     public static String SQL_DELETE_USER = "DELETE FROM \"reflection-task\".users WHERE id = ?";
